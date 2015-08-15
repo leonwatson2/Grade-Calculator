22253d8e2d30ab78305be71b6a8233deb8f2582c
vlw0052 <vernonwatzon@yahoo.com>
2015-08-15 13:59:10 -0500

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,4 +1,9 @@
 <?php 
+	
+	/*Signin php that returns 1 or 2 if user info
+	* does not match anything in database, returns
+	* "ook" if user information is correct, and 
+	*/
 	include("mysql/server.php");
 	session_start();
 	$link = mysqli_connect($server, $username, $passw, $username);
