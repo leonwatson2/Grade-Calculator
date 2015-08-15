df8bb1e325cc2ba0bd8a1e88ad708ff57cc3483f
vlw0052 <vernonwatzon@yahoo.com>
2015-08-15 13:56:01 -0500

    Update signout.php

--- signout.php
+++ signout.php
@@ -1,6 +1,10 @@
 <?php 
-session_start();
+/*Sign out php for user 
+*
+*/
+	session_start();
 	session_unset();
+	//updated is a cookie that tells index.php to change classes saved in cookies
 	setcookie("updated", false);
 	header("Location:http://grdcalc.com");
  ?>
